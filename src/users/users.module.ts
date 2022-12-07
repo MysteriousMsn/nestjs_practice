267a7e3d6f6376825e9f9a03b2a770af31aa074f
Mohsin Sunasara <mohsinsunasara60@gmail.com>
2022-12-07 11:18:59 +0530

    defined function in services and defined middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
+import { ExampleMiddleware } from './middlewares/example/example.middleware';
+import { UsersService } from './services/users/users.service';
 
 @Module({
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService]
 })
-export class UsersModule {}
+// we can register middleware like below
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // the example middleware will work on all users routes
+    consumer.apply(ExampleMiddleware).forRoutes('users');
+  }
+}
